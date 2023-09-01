cd85eca72548421d5f9a12e5b7b924964e3ce8e2
Kirill Pimenov <kirill@pimenov.cc>
2023-09-01 18:46:52 +0200

    Loads accounts and lists validators for the selected one

--- src/app.js
+++ src/app.js
@@ -1,25 +1,105 @@
-console.log("Hello from a single-file app!");
+import { ApiPromise, Keyring, WsProvider } from '@polkadot/api';
+import { web3Accounts, web3Enable } from '@polkadot/extension-dapp';
+const RPC_URL = 'wss://rpc.polkadot.io';
+const keyring = new Keyring();
 
 import './components/my-validator';
 import './components/my-spinner';
+import './components/my-address-chooser';
 import './components/my-validator-list';
 
-var suggestedValidators = [
+let suggestedValidators = [
     {name: 'Green Cloud', address: '1HtWJy6zTTc6Y1hyRTVpM6MDCpiWknsjDssUPC3FTKjfAGs'},
-    {name: 'DRAGONSTAKE 🐲', address: '1dGsgLgFez7gt5WjX2FYzNCJtaCjGG6W9dA42d9cHngDYGg'}
-];
-
-var currentValidators = [
+    {name: 'DRAGONSTAKE 🐲', address: '1dGsgLgFez7gt5WjX2FYzNCJtaCjGG6W9dA42d9cHngDYGg'},
     {name: 'Polkadot.pro - Realgar', address: '1REAJ1k691g5Eqqg9gL7vvZCBG7FCCZ8zgQkZWd4va5ESih'},
     {name: 'Ryabina', address: '14xKzzU1ZYDnzFj7FgdtDAYSMJNARjDc2gNw4XAFDgr4uXgp'},
     {name: 'General-Beck', address: '15MUBwP6dyVw5CXF9PjSSv7SdXQuDSwjX86v1kBodCSWVR7c'}
 ];
 
-var validatorList = document.getElementById('validator-list');
+const validatorList = document.getElementById('validator-list');
+validatorList.suggestedValidators = suggestedValidators;
 
-//Wait for 5 seconds before initializing the list
-new Promise(resolve => setTimeout(resolve, 5000)).then(() => {
-    validatorList.suggestedValidators = suggestedValidators;
-    validatorList.currentValidators = currentValidators;
+const addressChooser = document.getElementById('address-chooser');
+
+const TD = new TextDecoder();
+
+let allAccounts = [];
+let rpc_api = null;
+let selectedAccount = null;
+
+async function initWallets() {
+    const Wallet_Extensions = await web3Enable("Stake'n'Vote by The Kusamarian");
+    if (Wallet_Extensions.length === 0) {
+        addressChooser.initialized = true; // Needed so we can show the "No wallets found" message in the address chooser
+        return;
+    }
+    allAccounts = await web3Accounts();
+
+    addressChooser.accounts = allAccounts;
+    if (allAccounts.length > 0) {
+        addressChooser.onAccountChange = async (account) => {
+            selectedAccount = account;
+            getNominations(account.address);
+            console.log(selectedAccount.meta.name);
+        }
+        getNominations(allAccounts[0].address);
+    };
+    
+    addressChooser.initialized = true;
+}
+
+async function getIdentity(address) {
+    // We handle both cases of the address having an identity on its own,
+    // or it being a subidentity of another address with its own identity.
+    let identity = await rpc_api?.query.identity.identityOf(address);
+    if (!identity || !identity.isSome) {
+        identity = await rpc_api?.query.identity.superOf(address);
+        if (!identity || !identity.isSome) {
+            return;
+        }
+        // TODO: Handle the subidentity name together with the root identity name here somehow.
+
+        identity = await rpc_api?.query.identity.identityOf(identity.unwrap()[0]);
+        if (!identity || !identity.isSome) {
+            return;
+        }
+    }
+    identity = identity.unwrap();
+    // let judgements = await rpc_api?.query.identity.judgements(identity);
+    // if (!judgements || !judgements.isSome) {
+    //     return;
+    // }
+    // judgements = judgements.unwrap();
+    // // Only proceed if there is at least one "Reasonable" or "KnownGood" judgement.
+    // if (!(judgements.some(([, judgement]) => judgement.isReasonable || judgement.isKnownGood))) {
+    //     return;
+    // }
+
+    return {
+        address: address,
+        name: TD.decode(identity.info.display.asRaw),
+    }
+
+}
+
+async function getNominations(address) {
+    let nominators = await rpc_api?.query.staking.nominators(address);
+    if (!nominators || !nominators.isSome) {
+        return;
+    }
+    nominators = nominators.unwrap();
+    let targets = nominators.targets.map((target) => keyring.encodeAddress(target, 0)); // 0 is Polkadot SS58 format; don't forget to change it if you're using Kusama
+    targets = targets.map(async (target) => {
+        return await getIdentity(target);
+    });
+    targets = await Promise.all(targets);
+    validatorList.currentValidators = targets.filter((target) => target !== undefined);
     validatorList.initialized = true;
-})
+}
+
+async function initBlockchain() {
+    const RPC_Provider = new WsProvider(RPC_URL);
+    rpc_api = await ApiPromise.create({ provider: RPC_Provider });
+    await initWallets();
+}
+initBlockchain();
