52d747794967d8536b83ac806a97787746d9da27
Niklas P <contact@niklasplessing.net>
2023-12-15 16:35:20 +0100

    wip

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: "imgix",
+    path: 'the "domain" of your Imigix source',
+  },
+};
+
+module.exports = nextConfig;
