4559ee89801fa9d7e334838a2ccf7acd7f3b1513
Niklas P <contact@niklasplessing.net>
2023-12-15 17:49:54 +0100

    workflow

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 const basePath = isProd ? "/stake-n-vote" : "";
 
 const nextConfig = {
-  basePath,
+  basePath: basePath,
   output: "export",
   images: {
     unoptimized: true,
