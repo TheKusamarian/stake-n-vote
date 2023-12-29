d6af2468e14269a791e2328354ee7102d9a4dbbf
Niklas P <contact@niklasplessing.net>
2023-12-30 00:25:11 +0100

    basepath fix #2

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
-// const basePath = isProd ? "/stake-n-vote" : "";
+const basePath = isProd ? "/stake-n-vote" : "";
 
 const nextConfig = {
+  basePath: "",
   output: "export",
   distDir: "dist",
   images: {
