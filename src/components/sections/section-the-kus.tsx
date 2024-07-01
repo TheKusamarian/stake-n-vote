b45b67eb4ce6b26895ffb1547058b2cb7fec3170
Niklas P <contact@niklasplessing.net>
2024-07-01 15:32:13 +0200

    wip

--- src/components/sections/section-the-kus.tsx
+++ src/components/sections/section-the-kus.tsx
@@ -10,7 +10,7 @@ export async function SectionTheKus({ data }: { data: any }) {
     <section className="bg-gray-100 py-16" id="the-kus">
       <div className="container mx-auto px-4 mb-10">
         <h2 className="text-5xl font-bold text-center mb-4">
-          Ser, Have ya' Heard?
+          Ser, Have ya&apos; Heard?
         </h2>
         <p className="text-center mb-12">
           Too much happening with DOT? We got you covered with our latest
