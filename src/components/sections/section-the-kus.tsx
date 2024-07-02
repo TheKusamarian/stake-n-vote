c8a46cc5e1ae7f8cec96322b5346595c51f5a5a0
Niklas P <contact@niklasplessing.net>
2024-07-02 21:39:40 +0200

    add space monkeys section + revalidation

--- src/components/sections/section-the-kus.tsx
+++ src/components/sections/section-the-kus.tsx
@@ -8,9 +8,11 @@ import { VideoGridSkeleton } from "../videos/ViedeoGridSkeleton"
 export async function SectionTheKus({ data }: { data: any }) {
   return (
     <section className="bg-gray-100 py-16" id="the-kus">
-      <div className="container mx-auto px-4">
-        <h2 className="text-5xl font-bold text-center mb-4">The Kus</h2>
-        <p className="text-center mb-8">
+      <div className="container mx-auto px-4 mb-10">
+        <h2 className="text-5xl font-bold text-center mb-4">
+          Ser, Have ya&apos; Heard?
+        </h2>
+        <p className="text-center mb-16">
           Too much happening with DOT? We got you covered with our latest
           updates!
         </p>
--- src/components/videos/VideoGrid.tsx
+++ src/components/videos/VideoGrid.tsx
@@ -4,9 +4,13 @@ import LazyYoutubeEmbed from "../lazy-yt-embed"
 export function VideoGrid({
   videos,
   className,
+  classNames,
 }: {
   videos: any
   className?: string
+  classNames?: {
+    item?: string
+  }
 }) {
   // return <pre>{JSON.stringify(videos, null, 2)}</pre>
   return (
@@ -15,17 +19,22 @@ export function VideoGrid({
     >
       {videos?.map((video: any) => {
         const description = video.description.split("\n")[0]
+        //remove " Ser, Have ya' Heard?" from title
+        video.title = video.title.replace("- Ser, Have ya' Heard?", "")
         return (
-          <div key={video.id} className="relative">
-            <div className="group relative overflow-hidden rounded-md p-2">
+          <div
+            key={video.id}
+            className={cn("relative bg-white rounded-lg", classNames?.item)}
+          >
+            <div className="group relative overflow-hidden rounded-t-lg p-2 shadow-sm hover:shadow-xl transition-shadow">
               <div className="absolute -left-1/2 -top-1/2 h-[200%] w-[200%] origin-center rounded-md bg-gradient-to-br from-primary-500 to-teal-500 p-2 group-hover:animate-spin_right"></div>
               <LazyYoutubeEmbed
                 previewImageUrl={video?.thumbnails?.maxres?.url}
                 videoId={video?.id}
               />
             </div>
-            <div className="mt-2 flex flex-col items-center justify-center">
-              <h3 className="text-base font-semibold leading-normal">
+            <div className="p-4 mt-2 flex flex-col items-center justify-center">
+              <h3 className="mb-2 text-2xl font-bold tracking-tight leading-tight">
                 {video.title}
               </h3>
               <p className="mb-2 mt-2 text-sm leading-normal">{description}</p>
