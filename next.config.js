1b77a18281800dd69bfc9c3d481fa9af38baed9f
Niklas P <contact@niklasplessing.net>
2024-07-04 23:40:46 +0200

    undo next exports

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-const basePath = isProd ? '/stake-n-vote' : ''
-
 const nextConfig = {
-  basePath: '',
-  output: 'export',
-  distDir: 'dist',
-  images: {
-    unoptimized: true,
-    path: `${basePath}/_next/image`,
-  },
   logging: {
     fetches: {
       fullUrl: true,
